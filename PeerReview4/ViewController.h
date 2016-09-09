c0a4d7292c91ce47181249fe6133dba023eda7ed
user01 <user01@mac-mini-user01.local>
2016-09-09 06:56:11 +0300

    Initial Commit

---
+++ PeerReview4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PeerReview4
+//
+//  Created by user01 on 09.09.16.
+//  Copyright © 2016 user01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PeerReview4/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PeerReview4
+//
+//  Created by user01 on 09.09.16.
+//  Copyright © 2016 user01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
